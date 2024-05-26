7b442813312045cdbdf01e0af280dc80dab31b7a
Juan Fernández <juan.fernandezherrerin@uvigo.gal>
2024-05-26 09:52:00 +0200

    Incidencia timers

--- main.py
+++ main.py
@@ -138,6 +138,7 @@ for value in vals:
     report_info[value] = net.get_info_report()
 
     print('----------------')
+
     #Acumulate results in general dataframe in case we want evolution
     for key,detail in dc.items():
         if detail[0] == 'Capacity':
